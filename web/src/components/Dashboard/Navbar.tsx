269228c384d87c5771677a5c1a82521464ef6e03
guialves202 <guimacielalves2@gmail.com>
2023-10-26 00:44:27 -0300

    :sparkles: feat(component): dashboard navbar

---
+++ web/src/components/Dashboard/Navbar.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom"
+import { VscSearch, VscSignOut } from 'react-icons/vsc'
+import { IoAddCircleOutline } from 'react-icons/io5'
+import { FiEdit } from 'react-icons/fi'
+
+function Navbar() {
+  return (
+    <div className="bg-secondary text-black h-screen flex flex-col relative">
+      <div className="p-10">
+        <Link to={"/"}>
+          <h1 className="text-5xl text-yellow">Le Migliore Pizzas</h1>
+        </Link>
+      </div>
+
+      <div className="flex flex-col">
+        <Link to={"/dashboard/index"} className="text-xl text-white hover:bg-terceary hover:text-black pl-10 py-4 flex items-center gap-2 dashboard-active">
+          <VscSearch />
+          Verificar pedidos
+        </Link>
+
+        <Link to={"/dashboard/store"} className="text-xl text-white hover:bg-terceary hover:text-black pl-10 py-4 flex items-center gap-2">
+          <IoAddCircleOutline />
+          Registrar pedido
+        </Link>
+
+        <Link to={"/dashboard/update"} className="text-xl text-white hover:bg-terceary hover:text-black pl-10 py-4 flex items-center gap-2">
+          <FiEdit />
+          Modificar pedido
+        </Link>
+      </div>
+
+      <div className="absolute bottom-10 left-0 w-full">
+        <Link to={"/signout"} className="text-xl text-yellow hover:bg-terceary hover:text-black pl-10 py-4 flex items-center gap-2 w-full absolute bottom-0 left-0">
+          <VscSignOut />
+          Sair
+        </Link>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ web/src/components/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar"
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <Navbar />
+      <div>
+        <p>asdkapsdkad</p>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
