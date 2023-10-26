f35cca42a39480c88cbe957faeecbf9bedf5e58a
guialves202 <guimacielalves2@gmail.com>
2023-10-26 00:46:41 -0300

    :sparkles: feat(root): routes configuration

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Dashboard from './components/Dashboard'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: App(),
+  },
+  {
+    path: '/dashboard',
+    element: Dashboard()
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
