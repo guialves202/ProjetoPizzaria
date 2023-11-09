741520bcae8ab623cb62e0bc3cd259e92fbe0138
guialves202 <guimacielalves2@gmail.com>
2023-11-09 02:21:53 -0300

    :bug: fix: integration with api

--- web/src/components/Dashboard/Extra/ListExtra.tsx
+++ web/src/components/Dashboard/Extra/ListExtra.tsx
@@ -1,29 +1,63 @@
-function ListExtras() {
-  return (
-    <div className="flex flex-col gap-4 items-center p-6 bg-[#ddd] rounded w-[80%] h-[50%] overflow-auto">
-      <h1 className="text-2xl font-bold">Adicionais</h1>
-      <div className="flex flex-col gap-8 text-lg w-full">
-        <div className="flex items-center justify-around">
-          <span>ID</span>
-          <span>Adicional</span>
-          <span>Descrição</span>
-          <span>Preço</span>
-          <span>Alterar</span>
-          <span>Excluir</span>
-        </div>
+import { useState, useEffect } from "react"
+import { ExtraModel } from "../../../types/Models"
+import axios from "axios"
 
-        <div className="flex items-center justify-around">
-          <span>1</span>
-          <span>Borda catupiry</span>
-          <span>Borda recheada com catupiry</span>
-          <span>R$ 10.00</span>
-          <button className="bg-blue rounded py-[1px] px-3 text-white">Editar</button>
-          <button className="bg-primary rounded py-[1px] px-3 text-white">Excluir</button>
-        </div>
+export function ListExtras() {
+  const [extras, setExtras] = useState<ExtraModel[]>([])
+
+  useEffect(() => {
+    const getExtras= async () => {
+      try {
+        const fetchedExtras = await axios.get(`https://localhost:7118/api/Extra`)
+        setExtras(fetchedExtras.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    getExtras()
+  }, [])
+
+  return (
+    <div className="flex flex-col gap-4 items-center p-6 bg-[#ddd] rounded-xl w-[80%] h-[50%]">
+      <h1 className="text-2xl font-bold">Adicionais</h1>
+
+      <div className="overflow-auto flex flex-col items-center w-full">
+        <table className="list-table text-lg text-left align-middle w-[90%]">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Adicional</th>
+              <th>Descrição</th>
+              <th>Preço</th>
+              <th>Alterar</th>
+              <th>Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              extras.map((extra, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{extra.id}</td>
+                    <td>{extra.extra}</td>
+                    <td>{extra.description}</td>
+                    <td>R$ {extra.price.toString().includes('.') ? extra.price : `${extra.price.toString()}.00`}</td>
+                    <td>
+                      <button className="bg-blue rounded py-[1px] px-3 text-white">Editar</button>
+
+                    </td>
+                    <td>
+                      <button className="bg-primary rounded py-[1px] px-3 text-white">Excluir</button>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </div>
 
     </div>
   )
 }
-
-export default ListExtras
--- web/src/components/Dashboard/Flavor/ListFlavors.tsx
+++ web/src/components/Dashboard/Flavor/ListFlavors.tsx
@@ -1,62 +1,63 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { FlavorModel } from '../../../types/FlavorModel'
+import { FlavorModel } from '../../../types/Models'
 
-function ListFlavors() {
+export function ListFlavors() {
   const [flavors, setFlavors] = useState<FlavorModel[]>([])
 
   useEffect(() => {
-    const getData = async () => {
+    const getFlavors = async () => {
       try {
-        const response = await axios.get('https://localhost:7118/api/Flavor')
-        setFlavors(response.data)
-        console.log(flavors)
+        const fetchedFlavors = await axios.get(`https://localhost:7118/api/Flavor`)
+        setFlavors(fetchedFlavors.data)
       } catch (err) {
         console.log(err)
       }
     }
 
-    getData()
+    getFlavors()
   }, [])
 
-  if (flavors.length <= 0) {
-    return (
-      <div>Loading...</div>
-    )
-  }
-
   return (
-    <div className="flex flex-col gap-4 items-center p-6 bg-[#ddd] rounded w-[80%] h-[50%] overflow-auto">
+    <div className="flex flex-col gap-4 items-center p-6 bg-[#ddd] rounded-xl w-[80%] h-[50%]">
       <h1 className="text-2xl font-bold">Sabores</h1>
-      <div className="flex flex-col gap-8 text-lg w-full">
 
-        <div className="flex items-center justify-around">
-          <span>ID</span>
-          <span>Sabor</span>
-          <span>Descrição</span>
-          <span>Preço</span>
-          <span>Alterar</span>
-          <span>Excluir</span>
-        </div>
+      <div className="overflow-auto flex flex-col items-center w-full">
+        <table className="list-table text-lg text-left align-middle w-[90%]">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Sabor</th>
+              <th>Descrição</th>
+              <th>Preço</th>
+              <th>Alterar</th>
+              <th>Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              flavors.map((flavor, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{flavor.id}</td>
+                    <td>{flavor.flavor}</td>
+                    <td>{flavor.description}</td>
+                    <td>R$ {flavor.price.toString().includes('.') ? flavor.price : `${flavor.price.toString()}.00`}</td>
+                    <td>
+                      <button className="bg-blue rounded py-[1px] px-3 text-white">Editar</button>
 
-        {flavors.length > 0 ? flavors.map((flavor, index) => {
-          return (
-            <div className="flex items-center justify-around" key={index}>
-              <span>{flavor.Id}</span>
-              <span>{flavor.Flavor}</span>
-              <span>{flavor.Description}</span>
-              <span>R$ {flavor.Price}</span>
-              <button className="bg-blue rounded py-[1px] px-3 text-white">Editar</button>
-              <button className="bg-primary rounded py-[1px] px-3 text-white">Excluir</button>
-            </div>
-          )
-        }) : (
-          <div>Loading...</div>
-        )}
-
+                    </td>
+                    <td>
+                      <button className="bg-primary rounded py-[1px] px-3 text-white">Excluir</button>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </div>
+
     </div>
   )
 }
-
-export default ListFlavors
